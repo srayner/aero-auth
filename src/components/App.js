9df30b679cb79bd49664997100516559c72c9401
Steve Rayner <srayner02@gmail.com>
2021-01-30 21:57:05 +0000

    added routes and login function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Signup from './signup'
+import Login from './login'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../context/AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard'
 
 function App() {
   return (
-    <AuthProvider>
+    
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup/>
+          <Router>
+              <AuthProvider>
+                <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                  <Route path="/signup" component={Signup} />
+                  <Route path="/login" component={Login} />
+                </Switch>
+              </AuthProvider>
+          </Router>
+          
         </div>
       </Container>
-    </AuthProvider>
+    
   )
 }
 
